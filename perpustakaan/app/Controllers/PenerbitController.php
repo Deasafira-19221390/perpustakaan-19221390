a2d34093514fece5fe78f509bd6ef616f8adcc9f
deasfra <deasfra24@gmail.com>
2023-09-25 14:18:04 +0700

    penambahan controller penerbit

---
+++ perpustakaan/app/Controllers/PenerbitController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Controllers\BaseController;
+
+class PenerbitController extends BaseController
+{
+    public function index()
+    {
+        //
+    }
+
+    public function judul_buku(){
+
+    }
+
+    public function penulis(){
+
+    }
+
+    public function tahun_terbit(){
+
+    }
+        
+    }
